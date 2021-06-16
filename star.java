6015b55d0d9bdb0a364123613c5030baaa4332e4
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-06-16 10:57:48 +0530

    initial commit

---
+++ star.java
@@ -0,0 +1,11 @@
+public class star {
+    String name;
+
+    // public void main(String[] args) {
+    //     name = "Hello";
+    // }
+
+    star() {
+        name = "Hello";
+    }
+}
---
+++ stardb.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        int choice;
+        boolean check = true;
+        Scanner scannerObj = null;
+
+        // We do try-finally to close the scanner
+        // ( The class using which we get input )
+        try {
+            // We declare a scanner object to get user input
+            scannerObj = new Scanner(System.in);
+
+            System.out.println("\n*********Welcome to StarDB**********\n");
+
+            // Only run this code if the user
+            // enters a valid choice
+            while(check) {
+                check = false;
+
+                // Options
+                System.out.println(
+                        "1. Add A Star\n2. View A Star\n3. Update Details of a star\n4. Delete A Star\n5. View All Stars");
+
+                System.out.println("Enter your choice: ");
+
+                // Get the user input
+                choice = scannerObj.nextInt();
+
+                switch (choice) {
+                    // Add a star
+                    case 1:
+                        System.out.println("Add");
+                        break;
+
+                    // View a star
+                    case 2:
+                        System.out.println("View");
+                        break;
+
+                    // Update a star
+                    case 3:
+                        System.out.println("update");
+                        break;
+
+                    // Delete a star
+                    case 4:
+                        System.out.println("del");
+                        break;
+
+                    // Read from the DB
+                    case 5:
+                        System.out.println("All");
+                        break;
+
+                    default:
+                        // Clear the console
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                        
+                        System.out.println("Invalid choice. Try Again!");
+                        check = true;
+                        break;
+                }
+            }
+        } finally {
+            if (scannerObj != null)
+                scannerObj.close();
+        }
+    }
+}
